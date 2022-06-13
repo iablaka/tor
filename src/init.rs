22ae978e5272b56020bfd4ee4490a90a05e91b3e
Christophe Leborgne <ch.leborgne@gmail.com>
2022-06-13 15:50:07 +0200

    Update init.rs

--- src/init.rs
+++ src/init.rs
@@ -1,15 +1,20 @@
 use std::path::Path;
 use std::fs;
 
+const TOR_DIR: &str = "/.tor";
+const PROVIDERS_DIR: &str = "/providers";
+
 pub fn create_tor_folder(root_dir: &str) -> std::io::Result<()> {
     let mut path = String::from(root_dir);
-    path.push_str("/.tor");
+    path.push_str(TOR_DIR);
     if Path::new(&path).is_dir() {
         fs::remove_dir_all(&path).unwrap();
     }
-    // let mut providers_dir: String = path;
-    // providers_dir.push_str("/providers");
-    fs::create_dir(path)?;
-    // fs::create_dir(providers_dir);
+    fs::create_dir(&path)?;
+    path.push_str(PROVIDERS_DIR);
+    if Path::new(&path).is_dir() {
+        fs::remove_dir_all(&path).unwrap();
+    }
+    fs::create_dir(&path)?;
     Ok(())
 }
